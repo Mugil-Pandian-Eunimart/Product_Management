c07538ebecba4d614109734b4adbe949ee6d2667
Mugil-Pandian-Eunimart <mugil.pandian@eunimart.com>
2021-07-27 17:48:41 +0530

    Updated Function Structure

--- app/models/product.js
+++ app/models/product.js
@@ -1,6 +1,22 @@
 const ProductFile = require('../utils/file-handler');
 
 class ProductModel {
+
+    
+    async flag(data) {
+        const flag = ProductFile.writeProductData(data);
+        if(flag)
+            return true;
+        else
+            return false;
+    }
+
+    async checkProduct(data,id) {
+        const findProduct = data.find(product => product.id === id)
+        if(!findProduct) 
+            return false;
+    }
+
     async getProduct(id) {
         const productData = await ProductFile.readProductData();
         const findProduct = productData.find(product => product.id === id)
@@ -18,38 +34,22 @@ class ProductModel {
     async addProduct(data) {
         var createProduct = await ProductFile.readProductData();
         createProduct.push(data)
-        const flag = ProductFile.writeProductData(createProduct);
-        if(flag)
-            return true;
-        else
-            return false;
+        return this.flag(createProduct);
     }
 
     async updateProduct(id,data) {
         var updateProduct = await ProductFile.readProductData();
-        const findProduct = updateProduct.find(product => product.id === id)
-        if(!findProduct)
-            return false;
+        this.checkProduct(updateProduct,id);
         const filterProduct = updateProduct.filter(product => product.id !== id)
         filterProduct.push(data);
-        const flag = ProductFile.writeProductData(filterProduct);
-        if(flag)
-            return true;
-        else
-            return false;
+        return this.flag(filterProduct);
     }
 
     async deleteProduct(id) {
         var deleteProduct = await ProductFile.readProductData();
-        const findProduct = deleteProduct.find(product => product.id === id)
-        if(!findProduct)
-            return false;
+        this.checkProduct(deleteProduct,id);
         const filterProduct = deleteProduct.filter(product => product.id !== id)
-        const flag = ProductFile.writeProductData(filterProduct);
-        if(flag)
-            return true;
-        else
-            return false;
+        return this.flag(filterProduct);
     }
 }
 
