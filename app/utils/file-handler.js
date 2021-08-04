7ffd4514318e84985a1cf69a2cd6b2eeadd169c9
Mugil Pandian V <87295284+MugilPandian-Eunimart@users.noreply.github.com>
2021-08-04 15:47:22 +0530

    Update file-handler.js

--- app/utils/file-handler.js
+++ app/utils/file-handler.js
@@ -2,14 +2,14 @@ const fs = require('fs');
 
 class ProductFile {
     async readProductData () {
-        const data = fs.readFileSync('/home/ubuntu/Product_Management/app/utils/products.json');
+        const data = fs.readFileSync('/home/ec2-user/Product_Management/app/utils/products.json');
         return JSON.parse(data);
     }
 
     async writeProductData (data) {
         const jsonData = JSON.stringify(data);
         // console.log(jsonData);
-        fs.writeFileSync('/home/ubuntu/Product_Management/app/utils/products.json',jsonData)
+        fs.writeFileSync('/home/ec2-user/Product_Management/app/utils/products.json',jsonData)
     }
 }
 
