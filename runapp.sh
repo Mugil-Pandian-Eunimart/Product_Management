352d6022ec06cda87fc8cd5b3af5b6a37cc5c813
EC2 Default User <ec2-user@ip-172-31-28-126.us-east-2.compute.internal>
2021-08-05 08:17:25 +0000

    working service

--- runapp.sh
+++ runapp.sh
@@ -1,2 +1,3 @@
-#!/usr/bin/env node
-node /home/ec2-user/Product_Management/app.js
+#!/bin/bash
+cd /home/ec2-user/Product_Management
+NODE_PATH=app DEBUG=Product_Management:* /home/ec2-user/.nvm/versions/node/v16.6.1/bin/node ./bin/www
